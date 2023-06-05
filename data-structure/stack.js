1e8274f8bb1e35cdf70e0cc044524a1e4559984c
pramotepon <40195236+pramotepon@users.noreply.github.com>
2023-06-05 21:59:35 +0700

    Add new node to top stack

--- data-structure/stack.js
+++ data-structure/stack.js
@@ -7,12 +7,25 @@ class Node {
 }
 // Create class stack
 class Stack {
-    constructor(value){
+    constructor(value) {
         const newNode = new Node(value)
         this.top = newNode;
         this.length = 1;
     }
+    // Add new node to top stack
+    push(value) {
+        const newNode = new Node(value);
+        if (this.length === 0) {
+            this.top = newNode;
+        } else {
+            newNode.next = this.top;
+            this.top = newNode;
+        }
+        this.length++;
+        return this;
+    }
 }
 
 let myStack = new Stack(2);
+// console.log(myStack.push(3));
 console.log(myStack);
