7c81399b4c38af98452f915640171becc7313006
pramotepon <40195236+pramotepon@users.noreply.github.com>
2023-06-05 20:35:05 +0700

    Remove node from link list by index

--- data-structure/linked-list.js
+++ data-structure/linked-list.js
@@ -100,6 +100,20 @@ class LinkedList {
         this.length++;
         return true;
     }
+    // Remove node from link list by index
+    remove(index) {
+        if (index < 0 || index >= this.length) return false;
+        if (index === 0) return this.shift();
+        if (index === this.length - 1) return this.pop();
+
+        // get node befor remove index
+        const before = this.get(index - 1);
+        const temp = before.next;
+        before.next = temp.next;
+        temp.next = null;
+        this.length--;
+        return temp;
+    }
 }
 
 let myLinkedList = new LinkedList(1);
@@ -111,5 +125,6 @@ myLinkedList.push(3);
 // myLinkedList.shift();
 // console.log(myLinkedList.set(1, 9));
 // console.log(myLinkedList.get(1));
-console.log(myLinkedList.insert(1, 9));
+// console.log(myLinkedList.insert(1, 9));
+// console.log(myLinkedList.remove(1));
 console.log(myLinkedList);
