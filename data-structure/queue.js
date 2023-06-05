61f38e9e732bb16810acaf5b7bc2fd9f2d402b55
pramotepon <40195236+pramotepon@users.noreply.github.com>
2023-06-05 23:21:25 +0700

    Queue Add new node

--- data-structure/queue.js
+++ data-structure/queue.js
@@ -12,7 +12,20 @@ class Queue {
         this.last = newNode;
         this.length = 1;
     }
+    // Add new node
+    enqueue(value){
+        const newNode = new Node(value);
+        if(this.length === 0){
+            this.first = newNode;
+            this.last = newNode;
+        }else{
+            this.last.next = newNode;
+            this.last = newNode;
+        }
+        this.length++;
+    }
 }
 
 let myQueue = new Queue(1);
+myQueue.enqueue(2);
 console.log(myQueue);
