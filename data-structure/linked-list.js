cf93d3cbd6d49f492b7eaf697ec83ebc47c3a13d
pramotepon <40195236+pramotepon@users.noreply.github.com>
2023-06-05 20:57:41 +0700

    Change order data in link list from the last

--- data-structure/linked-list.js
+++ data-structure/linked-list.js
@@ -114,6 +114,21 @@ class LinkedList {
         this.length--;
         return temp;
     }
+    // Change order data in link list from the last
+    reverse() {
+        let temp = this.head;
+        this.head = this.tail;
+        this.tail = temp;
+        let next = temp.next;
+        let prev = null;
+        for (let i = 0; i < this.length; i++) {
+            next = temp.next;
+            temp.next = prev;
+            prev = temp;
+            temp = next;
+        }
+        return this;
+    }
 }
 
 let myLinkedList = new LinkedList(1);
@@ -127,4 +142,5 @@ myLinkedList.push(3);
 // console.log(myLinkedList.get(1));
 // console.log(myLinkedList.insert(1, 9));
 // console.log(myLinkedList.remove(1));
+// console.log(myLinkedList.reverse());
 console.log(myLinkedList);
