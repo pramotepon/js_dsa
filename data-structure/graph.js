b55b42ca09cb40e6f3a50a4e4743c7297af41e84
pramotepon <40195236+pramotepon@users.noreply.github.com>
2023-06-06 10:38:06 +0700

    Graph Cancel link vertex by edge

--- data-structure/graph.js
+++ data-structure/graph.js
@@ -20,6 +20,15 @@ class Graph {
         }
         return false;
     }
+    // Cancel link vertex by edge
+    removeEdge(vertex1, vertex2) {
+        if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
+            this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter((v) => v !== vertex2);
+            this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter((v) => v !== vertex1);
+            return true;
+        }
+        return false;
+    }
 }
 let myGraph = new Graph();
 myGraph.addVertext("A");
