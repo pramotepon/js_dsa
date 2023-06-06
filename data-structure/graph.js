216f22a4ccaafe79b903ea62454ec85daa711b53
pramotepon <40195236+pramotepon@users.noreply.github.com>
2023-06-06 10:33:03 +0700

    Graph Link vertex by edge

--- data-structure/graph.js
+++ data-structure/graph.js
@@ -11,8 +11,18 @@ class Graph {
         }
         return false;
     }
+    // Link vertex by edge
+    addEdge(vertex1, vertex2) {
+        if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
+            this.adjacencyList[vertex1].push(vertex2);
+            this.adjacencyList[vertex2].push(vertex1);
+            return true;
+        }
+        return false;
+    }
 }
 let myGraph = new Graph();
 myGraph.addVertext("A");
+myGraph.addVertext("B");
+myGraph.addEdge("A", "B");
 console.log(myGraph);
-console.log(myGraph.addVertext("A"));
