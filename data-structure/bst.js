c4d999ecd6b370e6c1d3db5ee902585f69b8a105
pramotepon <40195236+pramotepon@users.noreply.github.com>
2023-06-06 08:21:33 +0700

    BST Add new node

--- data-structure/bst.js
+++ data-structure/bst.js
@@ -1,5 +1,5 @@
-class Node{
-    constructor(value){
+class Node {
+    constructor(value) {
         this.value = value;
         this.left = null;
         this.right = null;
@@ -10,6 +10,35 @@ class BinarySearchTree {
     constructor() {
         this.root = null;
     }
+    // Add new node
+    insert(value) {
+        const newNode = new Node(value);
+        if (this.root == null) {
+            this.root = newNode;
+            return this;
+        }
+        let temp = this.root;
+        while (true) {
+            if (newNode.value === temp.value) return undefined;
+            if (newNode.value < temp.value) {
+                if (temp.left === null) {
+                    temp.left = newNode;
+                    return this;
+                }
+                temp = temp.left;
+            } else {
+                if (temp.right === null) {
+                    temp.right = newNode;
+                    return this;
+                }
+                temp = temp.right;
+            }
+        }
+    }
 }
 let myBST = new BinarySearchTree();
+myBST.insert(57);
+myBST.insert(21);
+myBST.insert(76);
+
 console.log(myBST);
